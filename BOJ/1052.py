0914ba98dcc22892e27d4d8a6c5867a2b875479e
lkitty0302 <lkitty0302@gmail.com>
2021-06-07 18:12:21 +0900

    2021.06.07

---
+++ BOJ/1052.py
@@ -0,0 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+n, k = map(int, input().split())
+temp = n
+cnt = 0
+result = 0
+if n < k:
+    print(result)
+else:
+    while True:
+        cnt = 0
+        temp = n
+        while temp > 0:
+            if temp % 2 == 1:
+                cnt += 1
+        
+            temp = temp // 2
+
+        if cnt <= k:
+            break
+        else:
+            n += 1
+            result += 1
+
+print(result)
