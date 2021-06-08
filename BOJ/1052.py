55f4ce8b5e6f89e806546fc9da544c8724cc3ec3
lkitty0302 <lkitty0302@gmail.com>
2021-06-08 17:17:42 +0900

    2021.06.08

--- BOJ/1052.py
+++ BOJ/1052.py
@@ -5,22 +5,20 @@ n, k = map(int, input().split())
 temp = n
 cnt = 0
 result = 0
-if n < k:
-    print(result)
-else:
-    while True:
-        cnt = 0
-        temp = n
-        while temp > 0:
-            if temp % 2 == 1:
-                cnt += 1
-        
-            temp = temp // 2
 
-        if cnt <= k:
-            break
-        else:
-            n += 1
-            result += 1
+while True:
+    cnt = 0
+    temp = n
+    while temp > 0:
+        if temp % 2 == 1:
+            cnt += 1
+    
+        temp = temp // 2
+
+    if cnt <= k:
+        break
+    else:
+        n += 1
+        result += 1
 
 print(result)
---
+++ BOJ/16437.py
@@ -0,0 +1,50 @@
+import sys
+input = sys.stdin.readline
+
+def dfs(node, visit, cnt):
+
+    if visit[node] == True:
+        return 0
+
+    visit[node] = True
+    tmp = 0
+
+    for i in range(len(graph[node])):
+        nnode = graph[node][i]
+        tmp = dfs(nnode, visit, state[nnode])
+        if tmp > 0:
+            cnt += tmp
+
+    return cnt
+
+N = int(input())
+
+visit = [False for _ in range(N+1)]
+
+# 그래프
+graph = [[] for _ in range(N + 1)]
+
+# 양 또는 늑대 수
+state = [0 for _ in range(N+1)]
+
+for i in range(1, N):
+    t, a, p = input().split()
+
+     #양
+    if t == 'W':
+        a = int(a) * -1
+    
+    state[i + 1] = int(a)
+
+    graph[i + 1].append(int(p))
+    graph[int(p)].append(i + 1)
+
+answer = 0
+
+for i in range(0, N+1):
+    result = 0
+    tmp = dfs(i, visit, state[i])
+    if tmp > 0:
+        answer += tmp
+
+print(answer)
